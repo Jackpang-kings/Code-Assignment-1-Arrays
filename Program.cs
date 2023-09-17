56ac087364da54687d069eb5fd83d96efc211a8e
Jackpang-kings <jpang18@bi.kgapupils.uk>
2023-09-17 16:54:28 +0100

    20230917-update2

--- Program.cs
+++ Program.cs
@@ -1,9 +1,11 @@
 ﻿using System;
+using System.Runtime.InteropServices;
 namespace HelloWorld { 
-class Program { 
-static void Main(string[] args)
+class Program
 {
-    int[] array = new int[9];
+static void Main()
+{
+    int[] array = new int[10];
     int n = array.Length;
     Console.WriteLine("Enter " + n + " numbers in the array.");
     for (int i = 0; i < n ; i++)
@@ -14,6 +16,44 @@ static void Main(string[] args)
     {
         Console.Write("|" + array[i]);
     }
+
+    Console.WriteLine("");
+    Console.WriteLine("Enter the number you are searching for");
+    int searchN = 0;
+    searchN = Convert.ToInt32(Console.ReadLine());
+    int result = BinarySearch(array,searchN);
+    if (result == -1)
+    {
+        Console.WriteLine("Number is not found");
+    }
+    else
+    {   
+        result = result + 1;
+        Console.WriteLine("Number is found in " + result + " of the array");
+    }
+}
+
+static int BinarySearch(int[] array,int searchn)
+{
+    int first = 0;
+    int last = array.Length - 1;
+    while (first <= last)
+    {
+        int mid = first + last/2;
+        if (array[mid] == searchn)
+        {
+            return mid;
+        }
+        else if (array[mid] < searchn)
+        {
+            first = mid + 1;
+        }
+        else
+        { 
+            last = mid - 1;
+        }
+    }
+    return -1;
 }
 }
 } 
