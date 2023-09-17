2a468624e05cc94c8c9dc7c4ffb6dceed0c1ee0c
Jackpang-kings <jpang18@bi.kgapupils.uk>
2023-09-17 13:29:28 +0100

    20230917-update

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+namespace HelloWorld { 
+class Program { 
+static void Main(string[] args)
+{
+    int[] array = new int[9];
+    int n = array.Length;
+    Console.WriteLine("Enter " + n + " numbers in the array.");
+    for (int i = 0; i < n ; i++)
+    {
+        array[i] = Convert.ToInt32(Console.ReadLine());
+    }
+    for (int i = 0; i < n ; i++)
+    {
+        Console.Write("|" + array[i]);
+    }
+}
+}
+} 
+
